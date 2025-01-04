bb233fe9a42d2a2a3fa943bc6797a22952ad8dff
Facundo Reiseng <facu_carhue@hotmail.com>
2025-01-04 18:47:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -53,6 +53,3 @@ def donations():
 @app.route('/contact') 
 def contact():
     return render_template('contact.html')
-            
-if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=5000)
